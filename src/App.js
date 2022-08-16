97e19a23610bc39780111789dc298bb342814d87
AntonVoronezh <pstut@mail.ru>
2022-08-16 18:39:24 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [likes, setLikes] = useState(0);
+  const increment = () => setLikes(likes + 1);
+  const decrement = () => setLikes(likes - 1);
+
   return (
     <div className="App">
-    kk
+      {likes}
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
