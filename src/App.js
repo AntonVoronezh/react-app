7de97d08e58385bbaae6cd17a273d545a2943894
AntonVoronezh <pstut@mail.ru>
2022-08-16 18:42:34 +0300

    init

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 
 function App() {
   const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('');
+
   const increment = () => setLikes(likes + 1);
   const decrement = () => setLikes(likes - 1);
 
   return (
     <div className="App">
       {likes}
+        <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
     </div>
