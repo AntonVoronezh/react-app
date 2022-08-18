b3ecca4b6597b796fa1f81c68da4727d1a071b95
AntonVoronezh <pstut@mail.ru>
2022-08-18 19:28:14 +0300

    init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "./styles/App.css";
 import PostItem from "./compomemts/PostItem";
 import PostList from "./compomemts/PostList";
+import MyButton from "./compomemts/UI/button/MyButton";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -12,6 +13,11 @@ function App() {
 
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder='название'/>
+        <input type="text" placeholder='описаник'/>
+          <MyButton > 6777dthyj </MyButton>
+      </form>
        <PostList posts={posts}/>
     </div>
   );
